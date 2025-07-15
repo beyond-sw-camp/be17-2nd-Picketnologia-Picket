2c1094288bc17aedede25d5d8a0b5e033d688765
seol <norsors@nate.com>
2025-07-15 20:32:44 +0900

    feat: 마이페이지 및 회원 정보 수정페이지 연결

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,7 +17,7 @@
                 </div>
                 <div class="d-flex gap-2 align-items-center">
                     <RouterLink class="link-underline-light link-dark" to="/login">로그인</RouterLink>
-                    <RouterLink class="link-underline-light link-dark" to="/my-page">마이페이지</RouterLink>
+                    <RouterLink class="link-underline-light link-dark" to="/mypage1">마이페이지</RouterLink>
                     <RouterLink class="link-underline-light link-dark" to="/seller">판매자 관리 페이지</RouterLink>
                 </div>
             </div>
