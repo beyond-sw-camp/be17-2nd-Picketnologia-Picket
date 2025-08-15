0a2bd489296e1d4e7b568815a8c03a8c9afd50aa
sangwoo <lesw1216@gmail.com>
2025-08-15 22:01:28 +0900

    refactor: 라우팅 가드 설정 (#14)
    특정 페이지는 로그인한 사용자만 접근 할 수 있도록 설정

---
+++ src/stores/useUserStore.js
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useUserStore = defineStore('user', () => {
+  const idx = ref(0)
+  const nickname = ref('')
+
+  const isLogin = ref(false)
+
+  const login = (loginUser) => {
+    isLogin.value = true
+    idx.value = loginUser.idx
+    nickname.value = loginUser.nickname
+  }
+
+  const logout = () => {
+    isLogin.value = false
+    idx.value = 0
+    nickname.value = ''
+  }
+
+  return { idx, isLogin, login, logout }
+})
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -3,7 +3,9 @@ import { reactive } from 'vue';
 import api from '@/api/member'
 import Header from '@/components/auth/header.vue';
 import { useRouter } from 'vue-router';
+import { useUserStore } from '@/stores/useUserStore';
 const router = useRouter();
+const userStore = useUserStore();
 
 const loginUser = reactive({
     email: '',
@@ -13,6 +15,7 @@ const loginUser = reactive({
 const login = async () => {
     const response = await api.memberLogin(loginUser);
     if (response.success) {
+        userStore.login(response.data);
         router.push('/');
     } else {
         alert('로그인 실패: ' + response.message);
