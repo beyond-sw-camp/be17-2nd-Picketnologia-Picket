1d57b01be39153a891f4acc689e78bea2cff16aa
sangwoo <lesw1216@gmail.com>
2025-08-16 04:03:42 +0900

    refactor: 로그인 버튼 수정
    로그인이 되어있지않으면 로그인 버튼 표시
    로그인이 되어있다면 마이페이지, 판매자 페이지, 로그아웃 표시

---
+++ src/components/LoginNavBar.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { useUserStore } from '@/stores/useUserStore';
+import { ref } from 'vue';
+
+const userStore = useUserStore();
+
+const isDropdownOpen = ref(false);
+
+const toggleDropdown = () => {
+    isDropdownOpen.value = !isDropdownOpen.value;
+};
+
+</script>
+
+<template>
+    <RouterLink class="link-underline-light link-dark" to="/login" v-if="!userStore.isLogin">로그인</RouterLink>
+    <div class="dropdown" v-else>
+        <button class="btn dropdown-toggle border-0 btn-lg fw-bold" type="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            test
+        </button>
+        <ul class="dropdown-menu">
+            <li>
+                <RouterLink class="link-underline-light link-dark dropdown-item" to="/mypage">마이페이지</RouterLink>
+            </li>
+            <li>
+                <RouterLink class="link-underline-light link-dark dropdown-item" to="/seller">판매자 관리 페이지</RouterLink>
+            </li>
+            <li>
+                <button class="link-underline-light link-dark dropdown-item" @click="userStore.logout">로그아웃</button>
+            </li>
+        </ul>
+    </div>
+
+</template>
+
+<style scoped></style>
